e5d4ef58f8db8327fc31e50a883d76413253108c
morphl1n <lukakurdiani@gmail.com>
2023-02-17 05:15:07 +0400

    hopefully final commit

--- config/database.php
+++ config/database.php
@@ -1,19 +1,13 @@
-<?php 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'legolas');
-    define('DB_PASSWORD', '654321');
-    define('DB_NAME', 'feedback');
 
-    //create connection
-
-    $connect = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-
-    //check connection
-
-    if($connect->connect_error){
-        die('Connection Failure' . $connect->connect_error);
-    }
-
-
-
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
