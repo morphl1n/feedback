198e5b328ec84052d43b565ee9bb62a1acd5b57a
morphl1n <lukakurdiani@gmail.com>
2023-02-17 05:54:40 +0400

    an test

--- feedback.php
+++ feedback.php
@@ -3,14 +3,22 @@
 <?php
  $sql = 'SELECT * FROM feedbackform';
  $result = mysqli_query($conn, $sql);
+ if (!$result) {
+  die('Error: ' . mysqli_error($conn));
+}
  $feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+ 
+ 
+// Check for errors
 
 
 
 ?>
    
     <h2>Past Feedbacks
-      <?php echo $test ?>
+      <?php echo var_dump($feedback);
+      echo var_dump($result); ?>
     </h2>
   <?php foreach($feedback as $item): ?>
     <div class="card my-3 w-75">
