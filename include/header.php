4e4fc8767de1bb1aba0e380d2d0ee84c169baa69
morphl1n <lukakurdiani@gmail.com>
2023-02-17 05:38:22 +0400

    an test

--- include/header.php
+++ include/header.php
@@ -12,22 +12,22 @@
 <body>
   <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
     <div class="container">
-      <a class="navbar-brand" href="/feedback/index.php">Feedback</a>
+      <a class="navbar-brand" href="index.php">Feedback</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
          <li class="nav-item">
-              <a class="nav-link" href="/index.php'; ?>">Home</a>
+              <a class="nav-link" href="/index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="feedback.php"
+              <a class="nav-link" href="/feedback.php"
                 >Feedback</a
               >
             </li>
             <li class="nav-item">
-              <a class="nav-link" href='/about.php'; ?>"
+              <a class="nav-link" href="/about.php"
                 >About</a
               >
             </li>
